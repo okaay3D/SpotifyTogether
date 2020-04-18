eabf37b76049acccd2db592909d3b24e7e9cd0d9
Marc H <marc@okaay.net>
2020-04-18 20:55:41 +0200

    replaced more #

--- index.js
+++ index.js
@@ -121,13 +121,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
+        res.redirect('/?' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
       } else {
-        res.redirect('/#' +
+        res.redirect('/?' +
           querystring.stringify({
             error: 'invalid_token'
           }));
